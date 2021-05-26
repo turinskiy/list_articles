fd66888ba20390f0cddad72b30324e0f09061299
Sergey Turinskiy <turinskiy@gmail.com>
2021-05-26 23:06:17 +0300

    clean up code in App component

--- src/App.js
+++ src/App.js
@@ -26,18 +26,16 @@ function App() {
 			)
 	}, [])
 
-	if (error) {
-		return <div>{`Error: ${error.message}`}</div>
-	}
-	else if (!isLoaded) {
-		return <div>{'Loading...'}</div>
-	}
-
 	return (
 		<div className="App">
 			<header className="App-header">
 				<div>
-					<ArticleList articles={articles}/>
+					{error 
+						? <div>{`Error: ${error.message}`}</div>
+						: !isLoaded 
+							? <div>{'Loading...'}</div>
+							: <ArticleList articles={articles}/>
+					}
 				</div>
 			</header>
 		</div>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,38 +1,9 @@
-import { useEffect, useState } from "react";
 import { ArticleRow } from './ArticleRow'
 import './styles/common_styles.css';
 import './styles/articles.css';
 
 export function ArticleList(props) {
 	const {articles} = props;
-	// const [error, setError] = useState(null);
-	// const [isLoaded, setIsLoaded] = useState(false);
-	// const [articles, setArticles] = useState([]);
-
-	// useEffect(() => {
-	// 	setIsLoaded(false);
-
-	// 	fetch('https://storage.googleapis.com/aller-structure-task/test_data.json')
-	// 		.then(res => res.json())
-	// 		.then(
-	// 			(result) => {
-	// 				// console.log(result[0])
-	// 				setIsLoaded(true);
-	// 				setArticles(result[0]);
-	// 			},
-	// 			(error) => {
-	// 				setIsLoaded(true);
-	// 				setError(error);
-	// 			}
-	// 		)
-	// }, [])
-
-	// if (error) {
-	// 	return <div>{`Error: ${error.message}`}</div>
-	// }
-	// else if (!isLoaded) {
-	// 	return <div>{'Loading...'}</div>
-	// }
 
 	return !articles || articles.length === 0 ? null : (
 		<div className="column grow1 container">
